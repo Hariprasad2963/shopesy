616059b5f8f7f1822e02c5ac77f9701d973dac5a
Hariprasad2963 <hariprasad2963@gmail.com>
2025-07-10 16:13:03 +0530

    app.js Modified: Message is changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get("/",(req, res) => {
-    res.send("welcome to shopeasy!");
+    res.send("welcome to shopeasy From hari prasad!");
 })
 
 app.listen(PORT, () => {
